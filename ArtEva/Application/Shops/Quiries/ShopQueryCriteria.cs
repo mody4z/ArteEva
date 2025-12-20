7ef7d5956491c35f60b9324084ee1e37d86f8eee
minasalah555 <salahmina452@gmail.com>
2025-12-20 16:05:00 +0200

    Add Some Filters to Shop  and Some Criteria

---
+++ ArtEva/Application/Shops/Quiries/ShopQueryCriteria.cs
@@ -0,0 +1,13 @@
+﻿using ArtEva.Models.Enums;
+
+namespace ArtEva.Application.Shops.Quiries
+{
+    public class ShopQueryCriteria
+    {
+        public int? OwnerUserId { get; set; }
+        public ShopStatus? Status { get; set; }
+        public decimal? MinRating { get; set; }
+        public decimal? MaxRating { get; set; }
+        public string? Name { get; set; }
+    }
+}
